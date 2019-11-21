6d4b3b79d5139f03dae46ba2446fe22268c492f4
zhangxk <980137428@qq.com>
2019-11-21 17:21:25 +0800

    日常提交

--- admin-web/src/main/java/com/zxk/admin/biz/config/MybatisPlusConfig.java
+++ admin-web/src/main/java/com/zxk/admin/biz/config/MybatisPlusConfig.java
@@ -1,10 +1,24 @@
 package com.zxk.admin.biz.config;
 
+import com.baomidou.mybatisplus.core.parser.ISqlParser;
+import com.baomidou.mybatisplus.core.parser.ISqlParserFilter;
+import com.baomidou.mybatisplus.core.toolkit.PluginUtils;
+import com.baomidou.mybatisplus.extension.parsers.BlockAttackSqlParser;
 import com.baomidou.mybatisplus.extension.plugins.PaginationInterceptor;
+import com.zxk.core.filter.xss.SQLFilter;
+import net.sf.jsqlparser.statement.delete.Delete;
+import net.sf.jsqlparser.statement.select.*;
+import net.sf.jsqlparser.statement.values.ValuesStatement;
+import org.apache.ibatis.binding.MapperMethod;
+import org.apache.ibatis.reflection.MetaObject;
+import org.apache.ibatis.reflection.wrapper.ObjectWrapper;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * TODO
@@ -23,8 +37,8 @@ public class MybatisPlusConfig {
         PaginationInterceptor paginationInterceptor = new PaginationInterceptor();
         // 设置最大单页限制数量，默认 50 条，-1 不受限制
         paginationInterceptor.setLimit(50);
-
         return paginationInterceptor;
     }
+
 
 }
--- admin-web/src/main/java/com/zxk/admin/biz/web/UserController.java
+++ admin-web/src/main/java/com/zxk/admin/biz/web/UserController.java
@@ -1,15 +1,21 @@
 package com.zxk.admin.biz.web;
 
+import com.alibaba.fastjson.JSONObject;
 import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
+import com.baomidou.mybatisplus.core.metadata.IPage;
+import com.baomidou.mybatisplus.core.metadata.OrderItem;
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import com.zxk.admin.biz.dao.UserDao;
 import com.zxk.admin.biz.domain.User;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.ArrayList;
 import java.util.List;
 
 
@@ -20,6 +26,7 @@ import java.util.List;
  * Date:   2019年11月19日 16:24
  * @version 1.0
  */
+@Slf4j
 @Api(tags = "用户管理")
 @RestController
 @RequestMapping(value = "/user")
@@ -32,6 +39,17 @@ public class UserController {
     @ApiOperation(value = "获取用户列表")
     public List<User> getUserList() {
         QueryWrapper<User> queryWrapper = new QueryWrapper<>();
+        Page<User> page = new Page<>(2,2);
+        List<OrderItem> orderItems = new ArrayList<>();
+        OrderItem orderItem = new OrderItem();
+        orderItem.setColumn("id");
+        orderItem.setAsc(false);
+        orderItems.add(orderItem);
+
+        page.setOrders(orderItems);
+
+        IPage<User> page1 = userDao.selectPage(page, queryWrapper);
+        log.error(JSONObject.toJSONString(page1));
         return userDao.selectList(queryWrapper);
     }
 
