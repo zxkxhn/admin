a7b9bce6fad763175226abf18e678d099e96afbf
zhangxk <980137428@qq.com>
2019-11-25 21:18:57 +0800

    调整pom,单元测试能通过

---
+++ admin-web/src/test/java/com/zxk/admin/AdminTest.java
@@ -0,0 +1,14 @@
+package com.zxk.admin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdminTest{
+
+    @Test
+    void contextLoads() {
+        System.out.println(111);
+    }
+
+}
