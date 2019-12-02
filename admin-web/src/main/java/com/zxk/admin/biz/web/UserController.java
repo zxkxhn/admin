90606eaa29b687f5d8520be0c727a5b968c73d2c
zhangxk <980137428@qq.com>
2019-12-02 19:11:23 +0800

    合并

--- admin-web/src/main/java/com/zxk/admin/biz/web/UserController.java
+++ admin-web/src/main/java/com/zxk/admin/biz/web/UserController.java
@@ -1,17 +1,14 @@
 package com.zxk.admin.biz.web;
 
-import com.zxk.admin.biz.form.UserForm;
-import com.zxk.core.common.Result;
 import io.swagger.annotations.Api;
-import io.swagger.annotations.ApiImplicitParam;
-import io.swagger.annotations.ApiOperation;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.rocketmq.spring.core.RocketMQTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.messaging.support.MessageBuilder;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-import javax.validation.Valid;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
@@ -34,14 +31,6 @@ public class UserController {
     @Autowired
     private RocketMQTemplate rocketMQTemplate;
 
-    @ApiOperation(value = "新增用户", notes = "新增一个用户")
-    @ApiImplicitParam(name = "userForm", value = "新增用户form表单", required = true, dataType = "UserForm")
-    @PostMapping
-    public Result add(@Valid @RequestBody UserForm userForm) {
-        log.debug("name:{}", userForm);
-
-        return Result.success(userForm);
-    }
 
 
 
