3b89530f9542facf3b3f614971bbef67b5b5821b
zhangxk <980137428@qq.com>
2019-12-03 20:19:09 +0800

    配置分离成功，因为window10 PowerShell 启动不起来 卧槽

--- admin-web/src/main/java/com/zxk/admin/Application.java
+++ admin-web/src/main/java/com/zxk/admin/Application.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication(scanBasePackages = "com.zxk.*")
-public class AdminApplication {
+public class Application {
 
 	public static void main(String[] args) {
-		SpringApplication.run(AdminApplication.class, args);
+		SpringApplication.run(Application.class, args);
 	}
 
 }
