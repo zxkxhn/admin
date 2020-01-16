e8d3bce3d789bb115355b52ec5e3e75cab090a3c
zhangxk <980137428@qq.com>
2020-01-16 16:17:10 +0800

    优化shiro

--- admin-web/src/test/java/com/zxk/admin/AdminTest.java
+++ admin-web/src/test/java/com/zxk/admin/AdminTest.java
@@ -1,9 +1,15 @@
 package com.zxk.admin;
 
+import com.zxk.core.util.RedisUtils;
+import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class AdminTest {
 
-
+    @Test
+    public void aaa() {
+        RedisUtils.getSingleton().set("aaaa", 1111);
+        System.out.println( RedisUtils.getSingleton().get("aaaa"));
+    }
 }
