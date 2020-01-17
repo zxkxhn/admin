e64c293d11f5579f8d8500b30bdf45f344ab9862
zhangxk <980137428@qq.com>
2020-01-17 10:19:21 +0800

    优化Hash分库分表

---
+++ admin-web/src/main/java/com/zxk/admin/biz/dao/TestDao.java
@@ -0,0 +1,15 @@
+package com.zxk.admin.biz.dao;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.zxk.admin.biz.domain.SysUser;
+import com.zxk.admin.biz.domain.Test;
+
+/**
+ * TODO
+ *
+ * @author zhangxk
+ * @Email 980137428@qq.com
+ * Date:   2019年12月01日 11:44
+ */
+public interface TestDao extends BaseMapper<Test> {
+}
--- admin-web/src/test/java/com/zxk/admin/AdminTest.java
+++ admin-web/src/test/java/com/zxk/admin/AdminTest.java
@@ -1,7 +1,12 @@
 package com.zxk.admin;
 
 import cn.hutool.core.util.RandomUtil;
+import com.alibaba.fastjson.JSONObject;
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
+import com.baomidou.mybatisplus.core.metadata.IPage;
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import com.zxk.admin.biz.dao.SysUserDao;
+import com.zxk.admin.biz.dao.TestDao;
 import com.zxk.admin.biz.domain.SysUser;
 import com.zxk.core.util.RedisUtils;
 import org.junit.jupiter.api.Test;
@@ -9,27 +14,61 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import javax.annotation.Resource;
+import java.util.List;
+import java.util.concurrent.Executor;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 @SpringBootTest
 class AdminTest {
 
 
     @Resource
-    private SysUserDao sysUserDao;
+    private TestDao testDao;
+
+    private ExecutorService pool = Executors.newFixedThreadPool(2);
 
     @Test
     public void aaa() {
-        for (int i = 0; i < 5000; i++) {
-            SysUser sysUser = new SysUser();
-            sysUser.setSalt(RandomUtil.randomString(20));
-            sysUser.setUsername(RandomUtil.randomString(20));
-            sysUser.setPassword(RandomUtil.randomString(20));
-            sysUser.setEmail(RandomUtil.randomString(20));
-            sysUser.setMobile(RandomUtil.randomNumbers(11));
-            sysUser.setStatus(RandomUtil.randomInt(10));
-            sysUserDao.insert(sysUser);
+        for (int i = 0; i < 50000; i++) {
+            com.zxk.admin.biz.domain.Test test = new com.zxk.admin.biz.domain.Test();
+            testDao.insert(test);
         }
+    }
 
+    @Test
+    public void bbb() throws InterruptedException {
+
+        pool.submit(() -> {
+            QueryWrapper<com.zxk.admin.biz.domain.Test> queryWrapper = new QueryWrapper<>();
+            for (int i = 1; i < 5000; i++) {
+                Page<com.zxk.admin.biz.domain.Test> page = new Page<>(i, 20);
+                IPage<com.zxk.admin.biz.domain.Test> testList = testDao.selectPage(page, queryWrapper);
+                System.out.println(JSONObject.toJSONString(testList));
+            }
+        });
+
+        pool.submit(() -> {
+            QueryWrapper<com.zxk.admin.biz.domain.Test> queryWrapper = new QueryWrapper<>();
+            for (int i = 1; i < 5000; i++) {
+                Page<com.zxk.admin.biz.domain.Test> page = new Page<>(i, 20);
+                IPage<com.zxk.admin.biz.domain.Test> testList = testDao.selectPage(page, queryWrapper);
+                System.out.println(JSONObject.toJSONString(testList));
+            }
+        });
+
+        pool.submit(() -> {
+            QueryWrapper<com.zxk.admin.biz.domain.Test> queryWrapper = new QueryWrapper<>();
+            for (int i = 1; i < 5000; i++) {
+                Page<com.zxk.admin.biz.domain.Test> page = new Page<>(i, 20);
+                IPage<com.zxk.admin.biz.domain.Test> testList = testDao.selectPage(page, queryWrapper);
+                System.out.println(JSONObject.toJSONString(testList));
+            }
+        });
+
+
+        Thread.sleep(500000);
 
     }
+
 }
