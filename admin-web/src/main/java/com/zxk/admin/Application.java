5f73cb6f126742e75af0a6f21b3dfcb81f347a59
zhangxk <980137428@qq.com>
2020-04-22 18:08:44 +0800

    成功启动

--- admin-web/src/main/java/com/zxk/admin/Application.java
+++ admin-web/src/main/java/com/zxk/admin/Application.java
@@ -3,9 +3,11 @@ package com.zxk.admin;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @Slf4j
-@SpringBootApplication(scanBasePackages = "com.zxk.*")
+@SpringBootApplication
+@ComponentScan(basePackages = {"com.zxk.*","com.ss.*"})
 public class Application{
 
 	public static void main(String[] args) {
